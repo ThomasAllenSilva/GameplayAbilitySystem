49ba43c5f54d8244400831ae3c076d8409ee80ac
Thomas Allen <thomasallensilvagithub@gmail.com>
2024-10-14 19:52:34 -0300

    Added Attributes Accessors

--- Source/Aura/Private/AbilitySystem/AttributeSet/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AttributeSet/AuraAttributeSet.cpp
@@ -1,5 +1,14 @@
 // Thomas Learning Project
 
-
 #include "AbilitySystem/AttributeSet/AuraAttributeSet.h"
 
+UAuraAttributeSet::UAuraAttributeSet()
+{
+	InitHealth(100.0f);
+
+	InitMaxHealth(100.0f);
+
+	InitMana(50.0f);
+
+	InitMaxMana(50.0f);
+}
--- Source/Aura/Private/AbilitySystem/AttributeSet/AuraAttributeSet.h
+++ Source/Aura/Private/AbilitySystem/AttributeSet/AuraAttributeSet.h
@@ -3,15 +3,41 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
 #include "AttributeSet.h"
+
+#include "AbilitySystemComponent.h"
+
 #include "AuraAttributeSet.generated.h"
 
-/**
- * 
- */
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 UCLASS()
-class UAuraAttributeSet : public UAttributeSet
+class UAuraAttributeSet final : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UAuraAttributeSet();
+
+public:
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Health);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxHealth);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Mana);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxMana);
 };
