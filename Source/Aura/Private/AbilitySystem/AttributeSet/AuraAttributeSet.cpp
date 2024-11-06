d4d19b3955e208325539fd7064ed2c40e00923ba
Thomas Allen <thomasallensilvagithub@gmail.com>
2024-11-05 21:33:58 -0300

    Added Attributes Values Clamping

--- Source/Aura/Private/AbilitySystem/AttributeSet/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AttributeSet/AuraAttributeSet.cpp
@@ -12,3 +12,27 @@ UAuraAttributeSet::UAuraAttributeSet()
 
 	InitMaxMana(50.0f);
 }
+
+void UAuraAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	TryClampAttributeValueChange(Attribute, NewValue);
+}
+
+void UAuraAttributeSet::PreAttributeBaseChange(const FGameplayAttribute& Attribute, float& NewValue) const
+{
+	TryClampAttributeValueChange(Attribute, NewValue);
+}
+
+void UAuraAttributeSet::TryClampAttributeValueChange(const FGameplayAttribute& Attribute, float& NewValue) const
+{
+	//TODO: Change These Hard-Coded Clamp Values To Something More Flexible
+	if (Attribute == GetHealthAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, 100.0f);
+	}
+
+	if (Attribute == GetManaAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.0f, 100.0f);
+	}
+}
--- Source/Aura/Private/AbilitySystem/AttributeSet/AuraAttributeSet.h
+++ Source/Aura/Private/AbilitySystem/AttributeSet/AuraAttributeSet.h
@@ -24,6 +24,10 @@ class UAuraAttributeSet final : public UAttributeSet
 public:
 	UAuraAttributeSet();
 
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+
+	virtual void PreAttributeBaseChange(const FGameplayAttribute& Attribute, float& NewValue) const override;
+
 public:
 	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes")
 	FGameplayAttributeData Health;
@@ -40,4 +44,7 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes")
 	FGameplayAttributeData MaxMana;
 	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxMana);
+
+private:
+	void TryClampAttributeValueChange(const FGameplayAttribute& Attribute, float& NewValue) const;
 };
