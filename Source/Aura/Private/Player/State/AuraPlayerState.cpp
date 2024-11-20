efc395c6dee75ef40372ced58c093cac2bee40c3
Thomas Allen <thomasallensilvagithub@gmail.com>
2024-11-20 16:33:59 -0300

    Update AuraPlayerState.cpp

--- Source/Aura/Private/Player/State/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/State/AuraPlayerState.cpp
@@ -8,14 +8,6 @@
 
 #include "Player/Data/Gas/PlayerGasData.h"
 
-#include "UI/PlayerStatus/View/AuraPlayerStatusUserWidgetView.h"
-
-#include "UI/PickupsMessages/View/AuraPickupMessageUserWidgetView.h"
-
-#include "UI/PickupsMessages/Controller/AuraPickupWidgetController.h"
-
-#include "UI/PlayerStatus/Controller/AuraPlayerStatusWidgetController.h"
-
 AAuraPlayerState::AAuraPlayerState()
 {
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
@@ -37,38 +29,11 @@ void AAuraPlayerState::BeginPlay()
 {
 	Super::BeginPlay();
 
-	check(GetPawn());
+	checkf(GetPawn(), TEXT("NULL Pawn When Initing Ability Actor Info"));
 
 	AbilitySystemComponent->InitAbilityActorInfo(this, GetPawn());
 
 	PlayerGasData = NewObject<UPlayerGasData>(this);
 
 	PlayerGasData->InitializeData(AbilitySystemComponent, AttributeSet);
-
-	CreateWidgets();
-}
-
-//What I'm doing here is probably wrong. I should make these widgets creation more flexible.
-//Do I know how to do that? Not yet...
-void AAuraPlayerState::CreateWidgets()
-{
-	/*check(PlayerStatusWidgetDefinition.WidgetTemplate);
-
-	UAuraPlayerStatusWidgetController* WidgetController = NewObject<UAuraPlayerStatusWidgetController>(this);
-
-	WidgetController->SetGasData(PlayerGasData);
-
-	UAuraPlayerStatusUserWidgetView* Widget = UAuraUIBlueprintFunctionLibrary::CreateWidgetAtLayer_Internal<UAuraPlayerStatusUserWidgetView>(GetWorld(), PlayerStatusWidgetDefinition.WidgetTemplate, PlayerStatusWidgetDefinition.LayerTag);
-
-	Widget->SetWidgetController(WidgetController);
-
-	WidgetController->BroadcastInitialValues();
-
-	UAuraPickupWidgetController* PickupWidgetController = NewObject<UAuraPickupWidgetController>(this);
-	PickupWidgetController->Initialize(AbilitySystemComponent);
-
-	UAuraPickupMessageUserWidgetView* PickupMessageWidget = UAuraUIBlueprintFunctionLibrary::CreateWidgetAtLayer_Internal<UAuraPickupMessageUserWidgetView>(GetWorld(), PickupMessageWidgetDefinition.WidgetTemplate, PickupMessageWidgetDefinition.LayerTag);
-
-	PickupMessageWidget->SetWidgetController(PickupWidgetController);*/
-
 }
