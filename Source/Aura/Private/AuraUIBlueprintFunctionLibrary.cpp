90613b38355353394461a78a058acd93bf419698
Thomas Allen <thomasallensilvagithub@gmail.com>
2024-11-20 16:31:11 -0300

    refactor: Deleted Unused Files

--- Source/Aura/Private/AuraUIBlueprintFunctionLibrary.cpp
+++
@@ -1,9 +0,0 @@
-// Thomas Learning Project
-
-
-#include "AuraUIBlueprintFunctionLibrary.h"
-
-UUserWidget* UAuraUIBlueprintFunctionLibrary::CreateUserWidgetAtLayer(UObject* OwningObject, TSubclassOf<UUserWidget> UserWidgetClass, const FGameplayTag& LayerTag)
-{
-	return CreateWidgetAtLayer_Internal<UUserWidget>(OwningObject->GetWorld(), UserWidgetClass, LayerTag);
-}
