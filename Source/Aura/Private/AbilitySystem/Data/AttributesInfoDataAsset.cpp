cffe59f5da49b39b1fdd1f0fe00c1590eaee40b2
Thomas Allen <thomasallensilvagithub@gmail.com>
2024-12-12 20:34:37 -0300

    refactor: Replace checkf with ASSERT_CONDITION

--- Source/Aura/Private/AbilitySystem/Data/AttributesInfoDataAsset.cpp
+++ Source/Aura/Private/AbilitySystem/Data/AttributesInfoDataAsset.cpp
@@ -2,11 +2,12 @@
 
 #include "AbilitySystem/Data/AttributesInfoDataAsset.h"
 
+#include "Debug.h"
 FAuraAttributeInfo UAttributesInfoDataAsset::GetAttributeInfo(const FGameplayTag& AttributeTag) const
 {
 	const FAuraAttributeInfo* AttributeInfo = AttributesInfos.Find(AttributeTag);
 
-	checkf(AttributeInfo, TEXT("Could Not Find Attribute Info For Tag: %s"), *AttributeTag.GetTagName().ToString());
+	ASSERT_CONDITION(AttributesInfos.Contains(AttributeTag), FString::Printf(TEXT("Could Not Find Attribute Info For Tag: %s"), *AttributeTag.GetTagName().ToString()));
 
 	return *AttributeInfo;
 }
