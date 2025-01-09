6970607e7f0237dd5a540a8f160c39bb9f2cf578
Thomas Allen <thomasallensilvagithub@gmail.com>
2025-01-08 21:17:19 -0300

    feat: Update Enemy AbilitySystemComponent Type

--- Source/Aura/Private/Characters/Enemy/AuraEnemyCharacter.cpp
+++ Source/Aura/Private/Characters/Enemy/AuraEnemyCharacter.cpp
@@ -3,13 +3,13 @@
 
 #include "Characters/Enemy/AuraEnemyCharacter.h"
 
-#include "AbilitySystemComponent.h"
+#include "AbilitySystem/Component/AuraAbilitySystemComponent.h"
 
 #include "AbilitySystem/AttributeSet/AuraAttributeSet.h"
 
 AAuraEnemyCharacter::AAuraEnemyCharacter()
 {
-	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
 
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AuraAttributeSetBase");
 }
