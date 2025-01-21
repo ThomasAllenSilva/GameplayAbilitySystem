78019e25893190a5d2f9c2a95fb20e016c5dfe8d
Thomas Allen <thomasallensilvagithub@gmail.com>
2025-01-21 19:04:28 -0300

    feat: Add Reference To ClassConfiguration Inside GameMode

--- Source/Aura/Private/GameMode/AuraGameMode.h
+++ Source/Aura/Private/GameMode/AuraGameMode.h
@@ -6,18 +6,23 @@
 #include "GameFramework/GameModeBase.h"
 #include "AuraGameMode.generated.h"
 
-/**
- * 
- */
+class UCharacterClassConfigurationInfo;
+
 UCLASS()
 class AAuraGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
 public:
 	virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage);
+
+	FORCEINLINE UCharacterClassConfigurationInfo* GetCharacterClassConfigurationInfo() const { return CharacterClassConfigurationInfo; }
 
 protected:
 	UFUNCTION(BlueprintImplementableEvent)
 	void OnInitGame();
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UCharacterClassConfigurationInfo> CharacterClassConfigurationInfo;
 };
