e72025be963956393abf86c652bd2df4a1bcf32e
Thomas Allen <thomasallensilvagithub@gmail.com>
2025-02-10 20:35:33 -0300

    feat: Create AuraDamageGameplayAbility

---
+++ Source/Aura/Private/AbilitySystem/Ability/AuraDamageGameplayAbility.h
@@ -0,0 +1,17 @@
+// Thomas Learning Project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Ability/AuraGameplayAbility.h"
+#include "AuraDamageGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UAuraDamageGameplayAbility : public UAuraGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
