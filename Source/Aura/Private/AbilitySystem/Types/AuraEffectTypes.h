24103d3ad94b866ca147df8bdc5bf54a321ab607
Thomas Allen <thomasallensilvagithub@gmail.com>
2025-02-10 18:38:30 -0300

    feat: Initial AuraGameplayEffectContext Implementation

--- Source/Aura/Private/AbilitySystem/Types/AuraEffectTypes.h
+++ Source/Aura/Private/AbilitySystem/Types/AuraEffectTypes.h
@@ -5,7 +5,39 @@
 #include "AuraEffectTypes.generated.h"
 
 USTRUCT(BlueprintType)
-struct FAuraDamageEffectContext final : public FGameplayEffectContext
+struct FAuraGameplayEffectContext final : public FGameplayEffectContext
 {
 	GENERATED_BODY()
+
+public:
+	bool IsCriticalHit() const { return bIsCriticalHit; }
+	bool IsBlockedHit() const { return bIsBlockedHit; }
+	void SetIsCriticalHit(bool bInIsCriticalHit) { bIsCriticalHit = bInIsCriticalHit; }
+	void SetIsBlockedHit(bool bInIsBlockedHit) { bIsBlockedHit = bInIsBlockedHit; }
+
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		return StaticStruct();
+	}
+
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+
+protected:
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+};
+
+template<>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true
+	};
 };
