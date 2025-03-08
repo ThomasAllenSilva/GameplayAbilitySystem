6f5ee00b17710cf1a829f56417a16a9b87d037ea
Thomas Allen <thomasallensilvagithub@gmail.com>
2025-03-08 11:24:30 -0300

    feat: Create AuraGameplayAbility Base Class

---
+++ Source/Aura/Private/AbilitySystem/Ability/Base/AuraGameplayAbility.cpp
@@ -0,0 +1,5 @@
+// Thomas Learning Project
+
+
+#include "AbilitySystem/Ability/Base/AuraGameplayAbility.h"
+
---
+++ Source/Aura/Private/AbilitySystem/Ability/Base/AuraGameplayAbility.h
@@ -0,0 +1,17 @@
+// Thomas Learning Project
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ability/CommonGameplayAbility.h"
+#include "AuraGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UAuraGameplayAbility : public UCommonGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
