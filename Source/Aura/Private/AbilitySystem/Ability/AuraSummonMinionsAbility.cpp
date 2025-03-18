1306e7acae3900e285838e4f567c122653238a6f
Thomas Allen <thomasallensilvagithub@gmail.com>
2025-03-17 21:32:32 -0300

    feat: Initial Implementation

--- Source/Aura/Private/AbilitySystem/Ability/AuraSummonMinionsAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Ability/AuraSummonMinionsAbility.cpp
@@ -3,3 +3,26 @@
 
 #include "AbilitySystem/Ability/AuraSummonMinionsAbility.h"
 
+TArray<FVector> UAuraSummonMinionsAbility::GetSpawnLocations() const
+{
+	const FVector ActorLocation = GetAvatarActorFromActorInfo()->GetActorLocation();
+
+	const FVector ActorForward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+
+	const float IterationAngle = SpreadAngle / MinionsCount;
+
+	TArray<FVector> SpawnPoints;
+
+	const FVector InitialAngle = ActorForward.RotateAngleAxis(-SpreadAngle / 2, FVector::UpVector);
+
+	for (int32 i = 0; i < MinionsCount; i++)
+	{
+		//holy shit, I'm pretty tired. Please my self from the future, finish this.. please
+		FVector SpawnPoint = InitialAngle.RotateAngleAxis(IterationAngle * 0, FVector::UpVector);
+	}
+
+
+
+
+	return SpawnPoints;
+}
--- Source/Aura/Private/AbilitySystem/Ability/AuraSummonMinionsAbility.h
+++ Source/Aura/Private/AbilitySystem/Ability/AuraSummonMinionsAbility.h
@@ -10,8 +10,18 @@
  * 
  */
 UCLASS()
-class UAuraSummonMinionsAbility : public UAuraGameplayAbility
+class UAuraSummonMinionsAbility final : public UAuraGameplayAbility
 {
 	GENERATED_BODY()
 	
+private:
+	TArray<FVector> GetSpawnLocations() const;
+
+
+private:
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess))
+	float SpreadAngle;
+
+	UPROPERTY(EditDefaultsOnly, meta = (AllowPrivateAccess))
+	int MinionsCount;
 };
