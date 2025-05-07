11a37b487d80e5fe965a653b39f42dbd6685bcef
Thomas Allen <thomasallensilvagithub@gmail.com>
2025-05-07 19:24:56 -0300

    Replace Unnecessary AbilityTask By FunctionLibrary Method

--- Source/Aura/Private/AuraFunctionLibrary.cpp
+++ Source/Aura/Private/AuraFunctionLibrary.cpp
@@ -3,6 +3,8 @@
 
 #include "AuraFunctionLibrary.h"
 #include "GameMode/AuraGameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "CommonAbilityFunctionLibrary.h"
 
 int UAuraFunctionLibrary::GetXPRewardForEnemy(const UObject* WorldContextObject, const FGameplayTag& EnemyTag, const int EnemyLevel)
 {
@@ -13,4 +15,26 @@ int UAuraFunctionLibrary::GetXPRewardForEnemy(const UObject* WorldContextObject,
 	const AAuraGameMode* AuraGameMode = World->GetAuthGameMode<AAuraGameMode>();
 
 	return AuraGameMode->GetXPRewardForEnemy(EnemyTag, EnemyLevel);
+}
+
+FVector UAuraFunctionLibrary::GetCursorWorldLocation(const UObject* WorldContextObject)
+{
+	const UWorld* World = WorldContextObject->GetWorld();
+
+	const ULocalPlayer* LocalPlayer = World->GetFirstLocalPlayerFromController();
+
+	const APlayerController* PlayerController = LocalPlayer->GetPlayerController(World);
+
+	FVector WorldLocation;
+	FVector WorldDirection;
+
+	PlayerController->DeprojectMousePositionToWorld(WorldLocation, WorldDirection);
+
+	FHitResult HitResult;
+
+	const FVector EndTraceLocation = ( WorldDirection) * UE_MAX_FLT;
+
+	World->LineTraceSingleByChannel(HitResult, WorldLocation, EndTraceLocation, ECollisionChannel::ECC_Visibility);
+
+	return HitResult.ImpactPoint;
 }
--- Source/Aura/Private/AuraFunctionLibrary.h
+++ Source/Aura/Private/AuraFunctionLibrary.h
@@ -19,4 +19,8 @@ class UAuraFunctionLibrary final : public UBlueprintFunctionLibrary
 public:
 	/* Retrieves the XP reward for an enemy using its tag and level */
 	static int GetXPRewardForEnemy(const UObject* WorldContextObject, const FGameplayTag& EnemyTag, const int EnemyLevel);
+
+	/* Get the cursor location in the nearest object point in the mouse direction */
+	UFUNCTION(BlueprintCallable, meta = (DefaultToSelf = "WorldContextObject"))
+	static FVector GetCursorWorldLocation(const UObject* WorldContextObject);
 };
